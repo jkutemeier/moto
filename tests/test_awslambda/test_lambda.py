e04ee80582a13abbb552f35fa17fe2e2595c3b7e
Steve Pulec <spulec@gmail.com>
2016-07-24 09:42:32 -0400

    Revert "Remove HTTPHeaders from ResponseMetadata in some tests"

--- tests/test_awslambda/test_lambda.py
+++ tests/test_awslambda/test_lambda.py
@@ -123,7 +123,6 @@ def test_create_function_from_aws_bucket():
             "SubnetIds": ["subnet-123abc"],
         },
     )
-    result['ResponseMetadata'].pop('HTTPHeaders') # this is hard to match against, so remove it
     result.should.equal({
         'FunctionName': 'testFunction',
         'FunctionArn': 'arn:aws:lambda:123456789012:function:testFunction',
@@ -166,7 +165,6 @@ def test_create_function_from_zipfile():
         MemorySize=128,
         Publish=True,
     )
-    result['ResponseMetadata'].pop('HTTPHeaders') # this is hard to match against, so remove it
     result.should.equal({
         'FunctionName': 'testFunction',
         'FunctionArn': 'arn:aws:lambda:123456789012:function:testFunction',
@@ -216,7 +214,6 @@ def test_get_function():
     )
 
     result = conn.get_function(FunctionName='testFunction')
-    result['ResponseMetadata'].pop('HTTPHeaders') # this is hard to match against, so remove it
 
     result.should.equal({
         "Code": {
@@ -272,7 +269,6 @@ def test_delete_function():
     )
 
     success_result = conn.delete_function(FunctionName='testFunction')
-    success_result['ResponseMetadata'].pop('HTTPHeaders') # this is hard to match against, so remove it
     success_result.should.equal({'ResponseMetadata': {'HTTPStatusCode': 204}})
 
     conn.delete_function.when.called_with(FunctionName='testFunctionThatDoesntExist').should.throw(botocore.client.ClientError)
@@ -336,9 +332,7 @@ def test_list_create_list_get_delete_list():
     }
     conn.list_functions()['Functions'].should.equal([expected_function_result['Configuration']])
 
-    func = conn.get_function(FunctionName='testFunction')
-    func['ResponseMetadata'].pop('HTTPHeaders') # this is hard to match against, so remove it
-    func.should.equal(expected_function_result)
+    conn.get_function(FunctionName='testFunction').should.equal(expected_function_result)
     conn.delete_function(FunctionName='testFunction')
 
     conn.list_functions()['Functions'].should.have.length_of(0)
